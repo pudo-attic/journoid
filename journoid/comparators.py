eefe3ba39b00aa15aea4787df3970d214b867b5c
Friedrich Lindenberg <friedrich@pudo.org>
2012-11-25 18:09:58 +0000

    Better comparators

--- journoid/comparators.py
+++ journoid/comparators.py
@@ -1,3 +1,5 @@
+import Levenshtein
+
 from journoid import text
 
 def contains(source_value, reference_value, **kw):
@@ -5,7 +7,34 @@ def contains(source_value, reference_value, **kw):
     reference_value_norm = text.normalize_plain(reference_value)
     return reference_value_norm in source_value_norm
 
+def distance(source_value, reference_value, task=None, **kw):
+    threshold = float(task.get('threshold', 0.2))
+    source_value_norm = text.normalize_plain(source_value)
+    reference_value_norm = text.normalize_plain(reference_value)
+    length = max([len(source_value_norm), len(reference_value_norm)])
+    max_distance = int(length * threshold) + 1
+    #print [max_distance]
+    return Levenshtein.distance(reference_value_norm, source_value_norm) \
+            < max_distance
+
+
+def opencorporates_status(source_value, reference_value, **kw):
+    import requests
+    print "OC CHECK", [source_value]
+    res = requests.get('http://opencorporates.com/reconcile/gb',
+            params={'query': source_value})
+    if not len(res.json.get('result')):
+        return False
+    first = res.json.get('result')[0]
+    if first.get('score') < 70:
+        return False
+    res = requests.get(first.get('uri')+'.json')
+    company = res.json.get('company')
+    return company.get('current_status').strip().lower() != 'active'
+
 COMPARATORS = {
-    'contains': contains
+    'contains': contains,
+    'distance': distance,
+    'company_status': opencorporates_status
     }
 
--- journoid/process.py
+++ journoid/process.py
@@ -1,4 +1,5 @@
 from urllib2 import urlopen
+import sqlite3, os
 from pprint import pprint
 import hashlib
 from messytables import CSVTableSet, type_guess, \
@@ -37,8 +38,8 @@ def load_data(config):
 
 def format_record(title, record):
     message = u"####### %s ########\n\n" % title
-    for k, v in record.items():
-        if len(v.strip()):
+    for k, v in sorted(record.items()):
+        if v is not None and len(v.strip()):
             #message += u" %s: %s\n" % (k.encode('utf-8'), v.encode('utf-8'))
             message += u" [%s]: %s\n" % (k, v)
     message += "\n\n"
@@ -58,12 +59,26 @@ def make_event_signature(task, index, source, reference):
     sig = hashlib.sha1(repr(sig)).hexdigest()
     return sig
 
+def check_duplicate(config, task, signature):
+    c = config['_db'].cursor()
+    res = c.execute("""SELECT signature FROM notifications WHERE
+        signature = ? AND task = ? AND recipient = ?;""",
+        (signature, task.get('name'), task.get('notify')))
+    if res.fetchone() is not None:
+        return True
+    c.execute("""INSERT INTO notifications (signature,
+        task, recipient) VALUES (?, ?, ?);""",
+        (signature, task.get('name'), task.get('notify')))
+    config['_db'].commit()
+    return False
+
 def match(comparator, source, reference, source_config,
-          reference_config):
+          reference_config, task):
     source_field = source_config.get('field')
     source_value = source.get(source_field)
     reference_value = reference.get(reference_config.get('field'))
-    return source_value, comparator(source_value, reference_value)
+    return source_value, \
+        comparator(source_value, reference_value, task=task)
 
 
 def process_task(task, config):
@@ -78,14 +93,23 @@ def process_task(task, config):
     for i, source in enumerate(load_data(source_config)):
         for reference in references:
             source_value, result = match(comparator, source, reference,
-                           source_config, reference_config)
+                           source_config, reference_config, task)
             if result:
                 signature = make_event_signature(task, i, source, reference)
-                print signature
-                send_message(task, config, source, source_value, reference)
+                if not check_duplicate(config, task, signature):
+                    send_message(task, config, source, source_value, reference)
+                else:
+                    print "SKIPPING", signature
 
 
 def process_tasks(config):
+    db = sqlite3.connect(os.path.join(config.get('_prefix'), 'journoid.db'))
+    c = db.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS notifications
+                 (task TEXT, signature TEXT, recipient TEXT)""")
+    db.commit()
+    c.close()
+    config['_db'] = db
     for task in config.get('tasks', []):
         process_task(task, config)
 
